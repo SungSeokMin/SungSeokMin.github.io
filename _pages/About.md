34c75a075e63d89f6a73f1ac7dddc7bfd1b51bc7
SungSeokMin <jkl154527@gmail.com>
2021-01-30 15:29:55 +0900

    update Alogorithm

--- _pages/About.md
+++
@@ -1,8 +0,0 @@
----
-title: 'About'
-layout: single
-permalink: /about/
-author_profile: true
----
-
-## Happy Developer, SungSeokMin
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "LSCS-연속된 부분 배열의 합의 최대값",
+        "excerpt":"LSCS 문제설명 정수를 요소로 갖는 배열 주어진 배열의 연속된 부분 배열의 합을 구한다. 이 중 가장 큰 값을 리턴해야 한다. 배열의 모든 요소가 음수인 경우도 있다. let output = LSCS([1, 2, 3]); console.log(output); // --&gt; 6 output = LSCS([1, 2, 3, -4]); console.log(output); // --&gt; 6 LSCS([1, 2, 3, -4,...","categories": ["algorithm"],
+        "tags": [],
+        "url": "https://sungseokmin.github.io/algorithm/LSCS/",
+        "teaser": null
+      },{
+        "title": "최소시간으로 길찾기-완전탐색",
+        "excerpt":"Robot Path 문제설명 2차원 배열(room), 현재 지점(src), 도착 지점(dst) 1은 장애물, 0은 이동 가능한 통로를 의미한다. 로봇은 지도 위를 일분에 한 칸씩 상하좌우로 이동 할 수 있다. 로봇의 위치와 목표 지점이 함께 주어질 경우 로봇이 목표 지점까지 도달하는 데 걸리는 최소 시간을 리턴 let room = [ [0, 0, 0,...","categories": ["algorithm"],
+        "tags": [],
+        "url": "https://sungseokmin.github.io/algorithm/robotPath/",
+        "teaser": null
+      }]
