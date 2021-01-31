2e30c9ff9ab80a97d2e55a9fd10fe701097547ea
SungSeokMin <jkl154527@gmail.com>
2021-01-31 18:28:44 +0900

    http, node, express

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,30 @@ var store = [{
         "title": "LSCS-연속된 부분 배열의 합의 최대값",
         "excerpt":"LSCS 문제설명 정수를 요소로 갖는 배열 주어진 배열의 연속된 부분 배열의 합을 구한다. 이 중 가장 큰 값을 리턴해야 한다. 배열의 모든 요소가 음수인 경우도 있다. let output = LSCS([1, 2, 3]); console.log(output); // --&gt; 6 output = LSCS([1, 2, 3, -4]); console.log(output); // --&gt; 6 LSCS([1, 2, 3, -4,...","categories": ["algorithm"],
         "tags": [],
-        "url": "https://sungseokmin.github.io/algorithm/LSCS/",
+        "url": "https://sungseokmin.github.io/algorithm/LSCS-algorithm/",
         "teaser": null
       },{
         "title": "최소시간으로 길찾기-완전탐색",
         "excerpt":"Robot Path 문제설명 2차원 배열(room), 현재 지점(src), 도착 지점(dst) 1은 장애물, 0은 이동 가능한 통로를 의미한다. 로봇은 지도 위를 일분에 한 칸씩 상하좌우로 이동 할 수 있다. 로봇의 위치와 목표 지점이 함께 주어질 경우 로봇이 목표 지점까지 도달하는 데 걸리는 최소 시간을 리턴 let room = [ [0, 0, 0,...","categories": ["algorithm"],
         "tags": [],
-        "url": "https://sungseokmin.github.io/algorithm/robotPath/",
+        "url": "https://sungseokmin.github.io/algorithm/robotPath-algorithm/",
+        "teaser": null
+      },{
+        "title": "Express",
+        "excerpt":"Express ? 공식문서 참고 Node.js를 위한 빠르고 대중적인 프레임워크 Middleware를 사용할 수 있다는 큰 장점을 가지고 있다. 설치 방법 npm install express express를 이용한 server 구현 // express 모듈을 가져온다. const express = require('express'); const app = express(); app.get('/', (request, response) =&gt; { res.send('Hello World'); }) app.listen(3000, () =&gt; {...","categories": ["backend","Express"],
+        "tags": [],
+        "url": "https://sungseokmin.github.io/backend/express/Express-backend/",
+        "teaser": null
+      },{
+        "title": "HTTP",
+        "excerpt":"HTTP ? 웹 상에서 client와 server 간에 요청/응답으로 데이터를 주고 받을 수 있는 프로토콜 작동방식 client -&gt; (요청) -&gt; server Server -&gt; (응답) -&gt; client 특징 stateless(무상태성) : 이전 요청이나 다음 요청을 기억하지 않는다. ex) client가 server에게 고기를 먹으러 가자고 요청을 보내자 server는 알겠다고 응답을 한다. 응답을 받은 client는 언제갈까...","categories": ["backend","http"],
+        "tags": [],
+        "url": "https://sungseokmin.github.io/backend/http/HTTP-backend/",
+        "teaser": null
+      },{
+        "title": "Node.js",
+        "excerpt":"Node.js ? V8 JavaScript 엔진으로 빌드된 JavaScript 런타임 즉, 브라우저 외의 다른 환경에서도 사용할 수 있게 해주는 런타임 module 시스템을 구축하고 있다. http server가 내장되어 있다. 설치 방법 공식사이트를 통해 파일을 다운 받을 수 있다. node가 설치되면 npm(Node Package Manager)가 같이 설치 된다. 설치가 되면 터미널을 켠 후 node -v와...","categories": ["backend","node"],
+        "tags": [],
+        "url": "https://sungseokmin.github.io/backend/node/Node-backend/",
         "teaser": null
       }]
